d947a8a66bd9389460416269e1fa7a29b5394e36
w.s <watanabesenhsn@gmail.com>
2025-05-27 15:49:07 +0900

    アプリケーション_ナビ側(ニュータイプ)
    いったんすべてのファイルを消去し、ナビゲーション用のreactを新たに作成しました。
    sever.pyを動作させたのち、
    "npm start"で動作します。

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import MapViewer from "./MapViewer";
+
+function App() {
+  return (
+    <div>
+      <h1>地図表示アプリ</h1>
+      <MapViewer />
+    </div>
+  );
+}
+
+export default App;
