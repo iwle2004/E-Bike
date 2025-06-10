f5207692dd316bee7b47cae9ec5a933c3686248e
iwle2004 <bilguunkhureltulga0@gmail.com>
2025-06-10 13:37:51 +0900

    new set1

---
+++ src/assets/MainPage.js
@@ -0,0 +1,11 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router';
+function MainPage() {
+    return (
+        <div className="container">
+            <h2 className="main-title">Welcome to Maizuru Bike Rental</h2>
+            <p className="main-text">Explore Maizuru with our convenient bike rental service. Check available bikes on the map, and don’t forget to submit a photo after your ride!</p>
+        </div>
+    );
+  }
+
+export default MainPage;
