99194fe17da39a5786b1d6b79a9745e5026a3249
sidukinui <sidukiupu@outlook.com>
2025-07-02 17:10:40 +0900

    修正

--- backend/server.py
+++ backend/server.py
@@ -6,7 +6,7 @@ import json
 import time
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, origins=["https://e-bike-backendd.onrender.com"], supports_credentials=True)
 
 @app.route("/run-navigation", methods=["POST"])
 def run_navigation():
