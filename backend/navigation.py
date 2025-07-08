d83e3ed1354d424d58021ba8ac72d0efe6caf56e
sidukinui <sidukiupu@outlook.com>
2025-07-08 13:18:23 +0900

    ランダム機能

--- backend/navigation.py
+++ backend/navigation.py
@@ -47,7 +47,7 @@ end_point = (end_dict["lat"], end_dict["lon"]) #目的地
 Xs, Ys = start_point #start_pointを緯度,経度に分割
 Xe, Ye = end_point #end_pointを緯度,経度に分割
 
-# 相対距離計算関数
+# 相対地球距離計算関数
 def distance(lat1, lon1, lat2, lon2):
     # Haversine距離の簡易版（近似）
     R = 6371000  # 地球半径[m]
@@ -107,7 +107,7 @@ client = openrouteservice.Client(key="5b3ce3597851110001cf6248b9ea1dfdfdb7416eb9
 #経由地からランダムに経由地を抽出
 bottom_waypoints = 3 #最低の経由地数
 random_number = random.randint(1, len(data)) #経由地数以下のランダムな数生成
-if random_route :
+if random_route:
     if len(points) >= bottom_waypoints:
         selected_points = random.sample(points, random_number)
     else:
